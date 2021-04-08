c0f33106e2f922f7bb1851b9610822c603481716
kritnatchapong <punmr@kritnatchapongs-MacBook-Air.local>
2021-04-08 18:49:47 +0700

    Edit back database.js

--- database.js
+++ database.js
@@ -1,22 +1,8 @@
 const mysql =require('mysql2');
 
-switch (process.env.ENV) {
-    case "LOCAL":
-        conn = {
-            host: process.env.DB_LOCAL_URL ,
-            user: process.env.DB_LOCAL_USER ,
-            password: process.env.DB_LOCAL_PASSWORD ,
-            database: process.env.DB_LOCAL_DATABASE
-        }
-        break;
-    case "GCLOUD":
-        conn = {
-            socketPath: process.env.DB_GCLOUD_URL ,
-            user: process.env.DB_GCLOUD_USER ,
-            password: process.env.DB_GCLOUD_PASSWORD ,
-            database: process.env.DB_GCLOUD_DATABASE
-        }
-        break;
-}
-
-module.exports = mysql.createConnection(conn);
+module.exports = mysql.createConnection({
+    host: process.env.DB_URL ,
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_DATABASE
+});
